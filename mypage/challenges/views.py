c83c5346f73c868d0b30ba2723b51eaec84b4401
Ladan <ladan.rb@gmail.com>
2024-07-16 09:26:34 +0200

    added dynamic segment to hold month values

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -6,5 +6,27 @@ from django.http import HttpResponse
 def january(request):
     return HttpResponse("Eat no meat!")
 
+
 def february(request):
     return HttpResponse("Run for 15 minutes everyday!")
+
+
+def monthly_challenge(request, month):
+    challenges = {
+        'january': "Eat no meat",
+        'february': "Cycle for 15 minutes every day",
+        'march': "Read a book",
+        'april': "Take a 30-minute walk",
+        'may': "Drink 8 cups of water daily",
+        'june': "Meditate for 10 minutes",
+        'july': "Practice yoga",
+        'august': "Write in a journal",
+        'september': "Learn a new skill",
+        'october': "Cook a new recipe",
+        'november': "Volunteer for a day",
+        'december': "Practice gratitude daily",
+    }
+    if month.lower() not in challenges.keys():
+        return HttpResponse("This month is not supported!")
+    challenge_text = challenges[month.lower()]
+    return HttpResponse(challenge_text)
