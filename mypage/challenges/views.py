7e11f75d7bde3ae80d975492361bc584b0bd9edf
Ladan <ladan.rb@gmail.com>
2024-07-16 09:42:36 +0200

    urls support months in numbers as well and redirects the url to the months names.

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Eat no meat!")
+challenges = {
+    'january': "Eat no meat",
+    'february': "Cycle for 15 minutes every day",
+    'march': "Read a book",
+    'april': "Take a 30-minute walk",
+    'may': "Drink 8 cups of water daily",
+    'june': "Meditate for 10 minutes",
+    'july': "Practice yoga",
+    'august': "Write in a journal",
+    'september': "Learn a new skill",
+    'october': "Cook a new recipe",
+    'november': "Volunteer for a day",
+    'december': "Practice gratitude daily",
+}
 
 
-def february(request):
-    return HttpResponse("Run for 15 minutes everyday!")
+def monthly_challenge_by_number(request, month):
+    try:
+        months = list(challenges.keys())
+        redirect_month = months[month-1]
+        return HttpResponseRedirect("/challenges/" + redirect_month)
+    except:
+        return HttpResponse("This month is not supported!")
 
 
 def monthly_challenge(request, month):
-    challenges = {
-        'january': "Eat no meat",
-        'february': "Cycle for 15 minutes every day",
-        'march': "Read a book",
-        'april': "Take a 30-minute walk",
-        'may': "Drink 8 cups of water daily",
-        'june': "Meditate for 10 minutes",
-        'july': "Practice yoga",
-        'august': "Write in a journal",
-        'september': "Learn a new skill",
-        'october': "Cook a new recipe",
-        'november': "Volunteer for a day",
-        'december': "Practice gratitude daily",
-    }
     if month.lower() not in challenges.keys():
         return HttpResponse("This month is not supported!")
     challenge_text = challenges[month.lower()]
